42c108e93b17daa766ede7ef605ce50db5ef4950
adhilmuhammedd <mhdadhil1100@gmail.com>
2025-01-17 11:08:30 +0530

    Update login.dart

--- login/lib/login.dart
+++ login/lib/login.dart
@@ -12,7 +12,7 @@ class MyLogin extends StatelessWidget {
       backgroundColor: const Color.fromARGB(255, 76, 57, 243),
       body: Center(
         child: Container(
-          height: 401,
+          height: 400,
           width: 400,
           decoration: BoxDecoration(borderRadius: const BorderRadius.all(Radius.circular(30)),
             color: Colors.white,

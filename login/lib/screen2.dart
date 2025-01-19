e2f4ec89ab4b138096336b1ffafa091b1919f3c8
adhilmuhammedd <mhdadhil1100@gmail.com>
2025-01-19 14:49:24 +0530

    Update screen2.dart

--- login/lib/screen2.dart
+++ login/lib/screen2.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class Screen2 extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
